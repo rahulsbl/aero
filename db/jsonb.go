62021f6d79bc54850edc32b0c2b7b23eef99995e
Harsh K <hk@gmail>
2016-01-18 09:55:35 +0530

    jsonb refactoring

--- db/jsonb.go
+++ db/jsonb.go
@@ -8,14 +8,14 @@ import (
 // Add support for jsonb fields
 // http://www.booneputney.com/2015-06-18-gorm-golang-jsonb-value-copy/
 
-type JMap map[string]interface{}
+type Jsonb map[string]interface{}
 
-func (j JMap) Value() (driver.Value, error) {
+func (j Jsonb) Value() (driver.Value, error) {
 	str, err := json.Marshal(j)
 	return string(str), err
 }
 
-func (j *JMap) Scan(value interface{}) error {
+func (j *Jsonb) Scan(value interface{}) error {
 	if err := json.Unmarshal(value.([]byte), &j); err != nil {
 		return err
 	}
@@ -23,4 +23,4 @@ func (j *JMap) Scan(value interface{}) error {
 }
 
 // TODO:
-type JSlice []interface{}
+// type JSlice []interface{}
